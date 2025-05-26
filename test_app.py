d45ea733ce49b4e271282c19e5d4482eaa9ea512
Sindhura Sriram <sreeramsindhu@gmail.com>
2025-05-26 14:59:19 -0400

    tst out

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 from app import calculate_bmi
 def test_bmi_calculation():
-    assert calculate_bmi(70, 170) == 24.2
+    assert calculate_bmi(70, 170) == 24.22
     print("Test passed: BMI calculation is correct.")
 
